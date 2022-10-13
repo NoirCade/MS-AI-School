3a008dc08f8d48f1f047944982be0081c0922bdd
NoirCade <102659027+NoirCade@users.noreply.github.com>
2022-10-13 19:07:36 +0900

    Create 01. print.py

---
+++ 7일차/01. print.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print('Hello World')
+
+
+# In[3]:
+
+
+print('Hello "안녕하세요"')
+
+
+# In[4]:
+
+
+print("Hello '안녕하세요'")
+
+
+# In[6]:
+
+
+print('안녕','하세요','반갑습니다.')
+
+
+# In[7]:
+
+
+print('안녕'+'하세요'+'반갑습니다.')
+
+
+# In[8]:
+
+
+print("""
+태양을 향해서라
+그리하면 너는 그림자를 볼 수 없을 것이다.
+
+-헬렌켈러
+
+""")
+
+
+# In[10]:
+
+
+print('Hello \nnice to meet you ^^')
+
+
+# In[11]:
+
+
+a = 123
+b = '안녕하세요'
+
+print('a={} b={}'.format(a,b))
+
+
+# In[12]:
+
+
+a = 123
+b = '안녕하세요'
+
+print(f'a={a} b={b}')
+
+
+# In[ ]:
+
+
+
+
