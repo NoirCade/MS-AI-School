3c26defdc655ad1faefaefce0921bb245ee2da6d
NoirCade <noircade530@gmail.com>
2023-02-15 23:49:29 +0900

    93일차

---
+++ 93일차/new_sound.py
@@ -0,0 +1,19 @@
+import pyaudio
+import numpy as np
+
+# Parameters for the sine wave
+frequency = 18  # Hz
+duration = 20  # seconds
+sample_rate = 144000  # samples per second
+volume = 1  # range [0.0, 1.0]
+
+# Generate the sine wave
+num_samples = int(sample_rate * duration)
+samples = (np.sin(2 * np.pi * frequency * np.arange(num_samples) / sample_rate) * volume).astype(np.float32)
+
+# Play the sine wave
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paFloat32, channels=1, rate=sample_rate, output=True)
+stream.write(samples)
+stream.close()
+p.terminate()
